b0c473428ffa5daa5326591e8fabcceca425a8e1
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-08-30 15:20:02 +0300

    B2-Step1

---
+++ javascript-basics-02/step 1/Step1.js
@@ -0,0 +1,27 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     var imageElement = document.querySelector("#image1");
+
+//     imageElement.addEventListener("mouseover", (e)=>{
+//         e.target.setAttribute('src','./images/image1_2.jpg')
+//     });
+//     imageElement.addEventListener("mouseout", revertImage);
+
+    
+
+//     function revertImage() {
+//         // Revert the src attribute of the image on mouse out
+//         imageElement.setAttribute ("src','./images/image1_2.jpg") ; // Replace with the original image source
+//     }
+// });
+
+document.addEventListener('DOMContentLoaded',function(){
+
+    var imgElemet=document.getElementById("image1");
+
+    imgElemet.addEventListener("mouseover", (e)=>{
+        e.target.setAttribute("src","./images/image1_2.jpg  ")   } );
+
+        imgElemet.addEventListener("mouseout", (e)=>{
+            imgElemet.setAttribute("src", "./images/image1.jpg")
+        })
+});
