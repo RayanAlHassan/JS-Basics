a97b74ed980403ddb77b2f2a65c9a79a1c376758
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-08-30 12:40:04 +0300

    Step

--- javascript-basics-01/Step_4.js
+++ javascript-basics-01/Step_4.js
@@ -1,4 +1,27 @@
-var name = prompt("Please enter your name ");
-var surname  = prompt("Please enter your surname ");
-var city = prompt("Please enter your city ");
-alert("Nom:"+ name +"\nPrenom:"+ surname +"\nVillage:"+ city)
+
+
+// document.addEventListener("DOMContentLoaded",function(){
+//     var name = document.getElementById("name");
+//     var surname = document.getElementById("surname");
+//     var city = document.getElementById("city");
+//     var validateBtn= document.getElementById("validate")
+
+//     validateBtn.addEventListener("click", function(){
+//         var Username = name.value;
+//         var Surname = surname.value;
+//         var City =city.value;
+//         alert("Nom:"+ Username +"\nPrenom:"+ Surname+"\nVillage:"+ City)
+//     })
+//     })
+
+ function submiting(){
+        let name = document.forms["form1"]["name"].value;
+    let surname =document.forms["form1"]["surname"].value;
+    let city =document.forms["form1"]["city"].value;
+
+    window.alert("Nom:"+ name +"\nPrenom:"+ surname+"\nVillage:"+ city);
+
+ }
+ 
+
+ 
---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,6 @@
+function multiply(){
+    var num1 = document.forms["multple"]["first_number"].value;
+    var num2 = document.forms["multple"]["second_number"].value;
+
+    window.alert(num1*num2)
+}
