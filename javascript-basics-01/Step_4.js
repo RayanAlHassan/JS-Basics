8b9722ab9ff8ea88e0296720d1976ec7c145fcc2
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-08-30 10:23:37 +0300

    Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,4 @@
+var name = prompt("Please enter your name ");
+var surname  = prompt("Please enter your surname ");
+var city = prompt("Please enter your city ");
+alert("Nom:"+ name +"\nPrenom:"+ surname +"\nVillage:"+ city)
