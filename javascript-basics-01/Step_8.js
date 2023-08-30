679b05735146cf01ef826fd5cbcedb28eed31917
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-08-30 13:11:38 +0300

    Step_8

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,10 @@
+function form8(){
+    var age = document.forms["step8"]["age"].value;
+
+    if(age>18){
+        alert("you are over 18")
+    }
+    else{
+        alert("you are under 18")
+    }
+}
