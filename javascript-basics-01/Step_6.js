e07dcce336cc0ed0b2300345769f411e9753fb15
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-08-30 12:48:51 +0300

    Step_6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,7 @@
+function remidering(){
+    var num1 = document.forms["remider"]["first_number"].value;
+    var num2 = document.forms["remider"]["second_number"].value;
+
+    window.alert(num1 % num2);
+
+}
