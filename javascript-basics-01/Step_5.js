2dd69caf3011df1ba5d1af957fdefb0f4f429d87
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-08-31 11:20:10 +0300

    edit b1-step5  using pure js

--- javascript-basics-01/Step_5.js
+++ javascript-basics-01/Step_5.js
@@ -1,6 +1,16 @@
-function multiply(){
-    var num1 = document.forms["multple"]["first_number"].value;
-    var num2 = document.forms["multple"]["second_number"].value;
+// function multiply(){
+//     var num1 = document.forms["multple"]["first_number"].value;
+//     var num2 = document.forms["multple"]["second_number"].value;
 
+//     window.alert(num1*num2)
+// }
+
+
+document.addEventListener("DOMContentLoaded",function(){
+    let num1 = prompt("enter num 1");
+    let num2g = prompt("enter nume 2");
+  
+
+   
     window.alert(num1*num2)
-}
+})
