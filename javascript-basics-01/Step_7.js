1cbb9553ef45ac7377c79fd649b3fa8d56692abf
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-08-31 11:26:29 +0300

    edit b1-step7  using pure js

--- javascript-basics-01/Step_7.js
+++ javascript-basics-01/Step_7.js
@@ -1,7 +1,17 @@
-function form7(){
-    var shoe = document.forms["step7"]["shoe_size"].value;
-    var year = document.forms["step7"]["year"].value;
+// function form7(){
+//     var shoe = document.forms["step7"]["shoe_size"].value;
+//     var year = document.forms["step7"]["year"].value;
 
-    window.alert(((((shoe*2)+5)*50)-year)+1766);
+//     window.alert(((((shoe*2)+5)*50)-year)+1766);
 
-}
+// }
+
+
+document.addEventListener("DOMContentLoaded",function(){
+    let shoe = prompt("enter your size of shoe  ");
+    let year = prompt("enter your year of birth");
+  
+
+   
+    window.alert(((((shoe*2)+5)*50)-year)+1766)
+})
