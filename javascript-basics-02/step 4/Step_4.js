02f1fa644bfc07ed42628e5c8448c3484b757115
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-08-31 11:45:52 +0300

    b2_step4

--- javascript-basics-02/step 4/Step_4.js
+++ javascript-basics-02/step 4/Step_4.js
@@ -1,6 +1,15 @@
 let name = document.querySelector("#name");
 let surname = document.querySelector("#surname");
 let city = document.querySelector("#city");
-let reset = document.querySelector("#button");
+let reset = document.querySelector("button");
 
-reset.addEventListener("")
+reset.addEventListener("click",()=>{
+let yes = confirm("yes");
+if(yes){
+    name.value="";
+    surname.value="";
+    city.value="";
+}
+ 
+
+})
