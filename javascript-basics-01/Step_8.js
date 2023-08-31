a133fb9470cab84de252ea82a488369a3dd6067d
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-08-31 11:28:11 +0300

    edit b1-step8  using pure js

--- javascript-basics-01/Step_8.js
+++ javascript-basics-01/Step_8.js
@@ -1,10 +1,23 @@
-function form8(){
-    var age = document.forms["step8"]["age"].value;
+// function form8(){
+//     var age = document.forms["step8"]["age"].value;
 
+//     if(age>18){
+//         alert("you are over 18")
+//     }
+//     else{
+//         alert("you are under 18")
+//     }
+// }
+
+document.addEventListener("DOMContentLoaded",function(){
+    let age = prompt("enter your age");
     if(age>18){
-        alert("you are over 18")
-    }
-    else{
-        alert("you are under 18")
-    }
-}
+                alert("you are over 18")
+            }
+            else{
+                alert("you are under 18")
+            }
+
+   
+   
+})
